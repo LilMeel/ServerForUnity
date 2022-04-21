381992e51454397dc2f48d697ec86870b72240c0
LilMeel <goshan9772@mail.ru>
2022-04-22 00:42:51 +0300

    Begining

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ServerForGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Realm Server | ver. 0.1 beta alpha";
+            Console.WriteLine("Realm Server started...");
+            Console.WriteLine();
+
+            ServerObject server = new ServerObject();
+            Task serverTask = new Task(server.StartServer);
+            serverTask.Start();
+            serverTask.Wait();
+
+            Console.WriteLine("Realm Server stoped!");
+            Console.ReadKey();
+        }
+    }
+}
